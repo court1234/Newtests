9a85d042cb78574e2afcf2ee41897fb23ed2b426
Courtland Gibbs <cg068113@ohio.edu>
2018-01-20 11:35:59 -0500

    additon

--- testfile.py
+++ testfile.py
@@ -1,16 +1,17 @@
+
 print("this is a test that i can set up a file")
 x = 2
 y = 3
 z = x + y
 print(z)
 
-#mylist = []
-#mylist.append(1)
-#mylist.append(2)
-#mylist.append(3)
-#print(mylist[0])
-#for x in mylist:
-    #print(x)
+mylist = []
+mylist.append(1)
+mylist.append(2)
+mylist.append(3)
+print(mylist[0])
+for x in mylist:
+    print(x)
 
 name = "Hal"
 print("I'm afrid I can't do that %s" %name)
